b409a5a317e39e7700c3315f214bdd26153c39bc
parkgeuntae <parkgeuntae@MacBook-Pro-2.local>
2023-11-21 23:55:44 +0900

    [Initial commit] - Row, Column, Text

---
+++ app/src/main/java/com/example/compose_example/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.example.compose_example
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.compose_example.ui.theme.Compose_exampleTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Compose_exampleTheme {
+                // TODO : Surface 특징 정리하기
+                Surface(
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Column(modifier = Modifier
+                        .fillMaxSize()
+                        .background(color = Color.LightGray)
+                        .padding(16.dp),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center,
+                        
+                    ) {
+                        Text("Hello")
+                        Text("World")
+                    }
+                }
+            }
+        }
+    }
+}
