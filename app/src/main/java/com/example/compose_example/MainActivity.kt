c27322ee8b1940b5441a06cf4d9fe1c7b6d03e01
parkgeuntae <dymnam@naver.com>
2023-11-23 23:04:55 +0900

    [CardView, rememberSaveable 로 상태를 다루는 학습]

--- app/src/main/java/com/example/compose_example/MainActivity.kt
+++ app/src/main/java/com/example/compose_example/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.compose_example
 
+import android.health.connect.datatypes.ElevationGainedRecord
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -17,15 +19,31 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CardElevation
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.compose_example.ui.theme.Compose_exampleTheme
@@ -34,16 +52,56 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            LazyColumn(modifier = Modifier
-                .background(Color.Cyan)
-                .fillMaxWidth(),
-                contentPadding = PaddingValues(16.dp),
-                verticalArrangement = Arrangement.spacedBy(16.dp)
+            // Delegated Properties 라는 기법을 사용하면 -> var isFavorite = remember { mutableStateOf(false) }
+            // 아래와 같이 사용할 수 있다.
+            var isFavorite by rememberSaveable { mutableStateOf(false) }
+            ImageCard(modifier = Modifier
+                .background(color = Color.LightGray)
+                .fillMaxWidth(0.5f)
+                .padding(16.dp), isFavorite = isFavorite) {favorite ->
+                isFavorite = favorite
+            }
+        }
+    }
+}
+
+@Composable
+fun ImageCard(
+    modifier: Modifier = Modifier,
+    isFavorite: Boolean = false,
+    onTapFavorite: (Boolean) -> Unit,
+) {
+    Card(
+        modifier = modifier,
+        shape = RoundedCornerShape(16.dp),
+        elevation = CardDefaults.cardElevation(8.dp, 16.dp)
+    ) {
+        Box(modifier = Modifier.height(200.dp)) {
+            Image(
+                painter = painterResource(id = R.drawable.poster),
+                contentDescription = "Movie Poster",
+                contentScale = ContentScale.Crop
+            )
+            Column(modifier = Modifier
+                .fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                items(50) { item ->
-                    Text("글씨 $item")
+                Text(
+                    text = "Movie Title",
+                    style = MaterialTheme.typography.bodyMedium,
+                    color = if (isFavorite) Color.Red else Color.White
+                )
+                IconButton(onClick = { onTapFavorite(!isFavorite) }) {
+                    Icon(
+                        imageVector = if (isFavorite) Icons.Default.Favorite else Icons.Default.FavoriteBorder,
+                        contentDescription = "Favorite",
+                        tint = if (isFavorite) Color.Red else Color.White
+                    )
+                    
                 }
             }
         }
+
     }
 }
