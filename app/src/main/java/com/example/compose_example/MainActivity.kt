237ff6e01a8eb8f2dbf332dc72e93078b6e6fbed
parkgeuntae <dymnam@naver.com>
2023-11-23 22:15:35 +0900

    [Column, LazyColumn 학습]

--- app/src/main/java/com/example/compose_example/MainActivity.kt
+++ app/src/main/java/com/example/compose_example/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -14,7 +15,10 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -30,18 +34,14 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Box(modifier = Modifier
-                .background(Color.Green)
-                .fillMaxWidth()
-                .height(200.dp)
+            LazyColumn(modifier = Modifier
+                .background(Color.Cyan)
+                .fillMaxWidth(),
+                contentPadding = PaddingValues(16.dp),
+                verticalArrangement = Arrangement.spacedBy(16.dp)
             ) {
-                Text(text = "Hello")
-                Box(modifier = Modifier
-                    .fillMaxSize()
-                    .padding(16.dp),
-                    contentAlignment = Alignment.BottomEnd
-                ) {
-                    Text(text = "World")
+                items(50) { item ->
+                    Text("글씨 $item")
                 }
             }
         }
